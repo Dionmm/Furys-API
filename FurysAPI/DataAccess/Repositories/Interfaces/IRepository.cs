6fdf24095438bcbfdcf6db3a6cce9b406679f9f7
Dion MacIntyre <dion@macintyres.me.uk>
2017-03-06 19:37:42 +0000

    Added Core Repository functionality to enable loose coupling of application

---
+++ FurysAPI/DataAccess/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FurysAPI.DataAccess.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ FurysAPI/DataAccess/Repositories/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using FurysAPI.DataAccess.DataContext;
+using FurysAPI.DataAccess.Repositories.Interfaces;
+
+namespace FurysAPI.DataAccess.Repositories
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly FurysApiDbContext Context;
+        private readonly DbSet<T> _entities;
+        protected Repository(FurysApiDbContext context)
+        {
+            Context = context;
+            _entities = Context.Set<T>();
+        }
+
+        public T Get(int id)
+        {
+            return _entities.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _entities.ToList();
+        }
+
+        public void Add(T entity)
+        {
+            _entities.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entity)
+        {
+            _entities.AddRange(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _entities.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            _entities.RemoveRange(entity);
+        }
+    }
+}
