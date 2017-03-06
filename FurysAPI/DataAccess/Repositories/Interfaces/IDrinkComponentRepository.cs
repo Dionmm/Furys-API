08a899b39d2c28d22a4629be100fac0e5d3e86aa
Dion MacIntyre <dion@macintyres.me.uk>
2017-03-06 19:43:43 +0000

    Added template repository interfaces for each of the entities

---
+++ FurysAPI/DataAccess/Repositories/Interfaces/IDrinkComponentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FurysAPI.DataAccess.Entities;
+
+namespace FurysAPI.DataAccess.Repositories.Interfaces
+{
+    public interface IDrinkComponentRepository: IRepository<DrinkComponent>
+    {
+    }
+}
