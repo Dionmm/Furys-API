2945b655ba63eba127bbda1dcdefe9bde8a83ff1
Dion MacIntyre <dion@macintyres.me.uk>
2017-03-06 21:50:23 +0000

    Implemented Unit of work functionality

---
+++ FurysAPI/DataAccess/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FurysAPI.DataAccess.Repositories.Interfaces;
+
+namespace FurysAPI.DataAccess
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        IBasketContentsRepository BasketContents { get; }
+        IDrinkComponentRepository DrinkComponents { get; }
+        IDrinkRecipeRepository DrinkRecipes { get; }
+        IDrinkRepository Drinks { get; }
+        IOrderRepository Orders { get; }
+
+        int Save();
+    }
+}
---
+++ FurysAPI/DataAccess/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FurysAPI.DataAccess.DataContext;
+using FurysAPI.DataAccess.Repositories;
+using FurysAPI.DataAccess.Repositories.Interfaces;
+
+namespace FurysAPI.DataAccess
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly FurysApiDbContext _context;
+
+        public UnitOfWork(FurysApiDbContext context)
+        {
+            _context = context;
+            //Add new Repositories in here
+            BasketContents = new BasketContentsRepository(_context);
+            DrinkComponents = new DrinkComponentRepository(_context);
+            DrinkRecipes = new DrinkRecipeRepository(_context);
+            Drinks = new DrinkRepository(_context);
+            Orders = new OrderRepository(_context);
+        }
+        
+        public IBasketContentsRepository BasketContents { get; }
+        public IDrinkComponentRepository DrinkComponents { get; }
+        public IDrinkRecipeRepository DrinkRecipes { get; }
+        public IDrinkRepository Drinks { get; }
+        public IOrderRepository Orders { get; }
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
